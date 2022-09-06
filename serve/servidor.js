68c04962a35cceddea2b695c1eb1e7093c085a1d
brasi16 <albertobiel00@hotmail.com>
2022-09-06 08:02:06 -0300

    Post e List

---
+++ serve/servidor.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password : "",
+    database : "locacao",
+});
+
+app.use(express.json());
+app.use(cors());
+
+//req= enviar res= RECEBAAAAAAAAAAAAAAAAAAAAAAAA
+app.post("/register", (req,res)=>{
+    const {modelo }= req.body;
+    const {marca }= req.body;
+    const {placa }= req.body;
+    const {km }= req.body;
+    
+    let mysql = "INSERT INTO carro(modelo, marca, placa, km) VALUES (?,?,?,?)";
+
+    db.query(mysql, [modelo, marca, placa,km], (err,result)=>{
+        res.send(result);
+    });
+});
+
+app.post("/search",(req,res)=>{
+    const {modelo }= req.body;
+    const {marca }= req.body;
+    const {placa }= req.body;
+    const {km }= req.body;
+
+    let mysql= "SELECT * from carro WHERE modelo = ? AND marca = ? AND placa = ? AND km = ?";
+
+    db.query(mysql , [modelo, marca, placa,km], (err, result)=>{
+        if (err) res.send(err);
+        res.send(result);
+    });
+});
+
+app.get("/list", (req,res)=>{
+    let mysql = "SELECT * FROM carro";
+    db.query(mysql, (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.put("/editar", (req,res)=>{
+    const {idcar} = req.body;
+    const {modelo }= req.body;
+    const {marca }= req.body;
+    const {placa }= req.body;
+    const {km }= req.body;
+
+    let mysql = "UPDATE carro SET modelo = ?, marca = ?, placa = ?, km = ? WHERE idcar= ?";
+    db.query(mysql, [modelo,marca,placa,km,idcar],(err,result)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+app.delete("/delete/:idcar",(req,res)=>{
+    const {idcar} = req.params;
+    let mysql = "DELETE FROM carro WHERE idcar = ?";
+    db.query(mysql, idcar,(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+
+app.listen(3002,()=>{
+    console.log("SIIIIIIIIIIIIIIIIIII");
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-function App() {
+
+export default function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const [values, setValues] = useState();
+  const [listCard, setListCard] = useState([]);
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleRegisterGame = () => {
+    console.log(values);
+    axios.post("http://localhost:3002/register", {
+        modelo: values.modelo,
+        marca: values.marca,
+        placa: values.placa,
+        km: values.km,
+    }).then(() => {
+        axios.post("http://localhost:3002/search", {
+            modelo: values.modelo,
+            marca: values.marca,
+            placa: values.placa,
+            km: values.km,
+        }).then((response) => {
+            setListCard([
+                ...listCard,
+                {
+                    idcar: response.data[0].idcar,
+                    modelo: values.modelo,
+                    marca: values.marca,
+                    placa: values.placa,
+                    km: values.km,
+                },
+            ]);
+        });
+    });
+};
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/list").then((response) => {
+      setListCard(response.data);
+    });
+  }, []);
+
+  const handleaddValues = (value) => {
+    setValues((prevValues) => ({
+        ...prevValues,
+        [value.target.id]: value.target.value,
+    }));
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+<Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="modelo"
+            label="Modelo"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleaddValues}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="marca"
+            label="Marca"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleaddValues}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="placa"
+            label="Placa"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleaddValues}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="km"
+            label="Km"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleaddValues}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleRegisterGame}>Cadastrar</Button>
+        </DialogActions>
+      </Dialog>
+
+      <div className="conteiner">
+      {
+        listCard.map((val) => {
+          return(
+            <>
+            <div className="list">
+          <div className="key" key={val.id}></div>
+          <div className="list-car">Id: {val.idcar}</div>
+          <div className="list-car">Modelo: {val.modelo}</div>
+          <div className="list-car">Marca: {val.marca}</div>
+          <div className="list-car">Placa: {val.placa}</div>
+          <div className="list-car">Km: {val.km}</div>
+          <button>Editar</button>
+          <button>Apagar</button>  
+          </div>
+
+
+          </>
+        )
+      }
+      )}
+
+      </div>
+       {/* <button onClick={mostrar}>Novo</button> */}
+       
+  </>
+      
   );
 }
-
-export default App;
